5c06852a22933f06b6ee7f53dcdadfee824f97af
David Saah <dasorange.hope@gmail.com>
2023-04-26 17:51:20 +0000

    add sample emission factor & add getEmissionsFactor method

--- App/src/Car.java
+++ App/src/Car.java
@@ -2,20 +2,20 @@ import java.util.HashMap;
 
 /**
  * A class that defines the properties of a car.
- * 
+ *
  */
 public class Car extends Vehicle {
     // TODO: What is the emissions factor for public transport?
     // TODO: Most public transportations use diesel fuel. What is the emissions
     // factor for diesel fuel?
-    public static final double PUBLIC_EMISSIONS_FACTOR = 0.0; // grams of CO2 per mile
+    public static final double PUBLIC_EMISSIONS_FACTOR = 120.0; // grams of CO2 per mile
 
     // TODO: What is the emissions factor for private transport?
     // TODO: It depends on the type of fuel used; whether diesel or gasoline.
-    public static final HashMap<String, Double> PRIVATE_EMISSIONS_FACTOR = new HashMap<>() {
+    private static final HashMap<String, Double> PRIVATE_EMISSIONS_FACTOR = new HashMap<>() {
         {
-            put("diesel", 0.0);
-            put("gasoline", 0.0);
+            put("diesel", 120.0);
+            put("gasoline", 400.0);
         }
     }; // grams of CO2 per mile
 
@@ -23,9 +23,9 @@ public class Car extends Vehicle {
 
     /**
      * Car constructor.
-     * 
+     *
      * Sets the name of the vehicle to "Car".
-     * 
+     *
      */
     public Car(String fuelType) {
         super("Car");
@@ -34,9 +34,21 @@ public class Car extends Vehicle {
 
     /**
      * Gets the type of fuel used by the car.
-     * 
+     *
      */
     public String getFuelType() {
         return this.fuelType;
+    }
+
+    /**
+     * Gets the emissions factor of the car.
+     *
+     */
+    public double getEmissionsFactor() {
+        if (this.getType()) {
+            return PUBLIC_EMISSIONS_FACTOR;
+        } else {
+            return PRIVATE_EMISSIONS_FACTOR.get(this.fuelType);
+        }
     }
 }
