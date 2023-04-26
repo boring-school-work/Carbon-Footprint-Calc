fba173e8272f93306ff913d332e82881b70f9061
David Saah <dasorange.hope@gmail.com>
2023-04-26 09:33:20 +0000

    add main class

---
+++ App/src/App.java
@@ -0,0 +1,15 @@
+/**
+ * App
+ */
+public class App {
+    // represents the type of vehicle
+    public enum Vehicles {
+        Car, Plane
+    }
+
+    public static void main(String[] args) {
+        System.out.println("Welcome to Transportation Optimization System\n");
+
+    }
+
+}
