f28bd2082487108c4322ae2c49e861c32163adab
David Saah <dasorange.hope@gmail.com>
2023-04-26 09:35:02 +0000

    test format of user data

---
+++ App/src/Test.java
@@ -0,0 +1,15 @@
+import java.util.HashMap;
+
+public class Test {
+    public static void main(String[] args) {
+        HashMap<String, HashMap<String, String>> data = new HashMap<>();
+        /**
+         * 
+         * Key: Person name
+         * vehicle name
+         * fuel type
+         * emissions factor
+         *
+         */
+    }
+}
