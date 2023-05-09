c2f68e450d7725601f4ec86757e06b019b69daa1
David Saah <dasorange.hope@gmail.com>
2023-05-09 15:18:55 +0000

    add documentation

--- src/Person.java
+++ src/Person.java
@@ -3,17 +3,28 @@ import java.util.HashMap;
 /**
  * Person class
  *
- * Represents a person who interacts with the program.
+ * Represents the user who interacts with the program.
+ *
+ * @author David Saah
+ * @version 1.0
+ * @since 2023-04-26
  *
  */
 public class Person {
     private String name; // the name of the person
-    private HashMap<String, Vehicle> vehiclesOwned = new HashMap<>(); // the vehicles owned by the person
+    /*
+     * The vehicles owned by the person
+     * HashMap<Actual name, vehicle object>
+     *
+     */
+    private HashMap<String, Vehicle> vehiclesOwned = new HashMap<>();
 
     /**
      * Person constructor
      *
      * Sets the name of the person.
+     *
+     * @param name the name of the person
      *
      */
     public Person(String name) {
@@ -23,6 +34,8 @@ public class Person {
     /**
      * Gets the name of the person.
      *
+     * @return the name of the person
+     *
      */
     public String getName() {
         return name;
@@ -31,13 +44,17 @@ public class Person {
     /**
      * Gets the vehicles owned by the person.
      *
+     * @return the vehicles owned by the person
+     *
      */
     public HashMap<String, Vehicle> getVehiclesOwned() {
         return vehiclesOwned;
     }
 
     /**
-     * Adds a vehicle to the vehicles owned by the person.
+     * Adds a new vehicle to the vehicles owned by the person.
+     *
+     * @param vehicle a new vehicle
      *
      */
     public void addVehicle(Vehicle vehicle) {
