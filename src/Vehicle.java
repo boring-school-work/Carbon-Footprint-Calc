4bbf7237c7523258581563883b76319e5614ace7
David Saah <dasorange.hope@gmail.com>
2023-05-09 15:18:47 +0000

    add documentation

--- src/Vehicle.java
+++ src/Vehicle.java
@@ -3,19 +3,23 @@
  *
  * Defines the general properties of a vehicle
  *
+ * @author David Saah
+ * @version 1.0
+ * @since 2023-04-26
+ *
  */
 public abstract class Vehicle {
     protected String name; // the name of the vehicle
     protected boolean isPublic; // whether the is public or privately owned
-    protected String altName; // the alternative name of the vehicle
+    protected String altName; // the actual name of the vehicle
     protected String fuelType; // the type of fuel used by the car
 
     /**
      * Vehicle constructor
      *
-     * @param name the name of the vehicle
-     * @param altName
-     * @param isPublic
+     * @param name     the name of the type of vehicle
+     * @param altName  the actual name of the Vehicle
+     * @param isPublic whether the vehicle is publicly or privately owned
      *
      */
     public Vehicle(String name, String altName, boolean isPublic) {
@@ -27,8 +31,8 @@ public abstract class Vehicle {
     /**
      * Second constructor that sets the name and type
      *
-     * @param name
-     * @param isPublic
+     * @param name     the name of the type of vehicle
+     * @param isPublic whether the vehicle is publicly or privately owned
      *
      */
     public Vehicle(String name, boolean isPublic) {
@@ -44,24 +48,22 @@ public abstract class Vehicle {
      */
     public boolean getType() {
         return this.isPublic;
-
     }
 
     /**
-     * Gets the name of the vehicle
+     * Gets the name of the type of vehicle
      *
-     * @return the name of the vehicle
+     * @return the name of the type of vehicle
      *
      */
     public String getName() {
         return this.name;
-
     }
 
     /**
-     * Sets the alternative name of the vehicle
+     * Sets the actual name of the vehicle
      *
-     * @param altName the alternative name of the vehicle
+     * @param altName the actual name of the Vehicle
      *
      */
     public void setAltName(String altName) {
@@ -69,7 +71,9 @@ public abstract class Vehicle {
     }
 
     /**
-     * Gets the alternative name of the vehicle
+     * Gets the actual name of the vehicle
+     *
+     * @return the actual name of the vehicle
      *
      */
     public String getAltName() {
