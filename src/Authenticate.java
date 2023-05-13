9ebba20906468aa42afea8c33e14da5d24f4a6eb
David Saah <dasorange.hope@gmail.com>
2023-05-13 00:05:00 +0000

    add documentation

--- src/Authenticate.java
+++ src/Authenticate.java
@@ -13,23 +13,42 @@ import javax.crypto.spec.PBEKeySpec;
  */
 public class Authenticate {
 
-    private static final int SIZE = 128;
-    private static final int ITERATIONS = 18;
-    private static final String ALGORITHM = "PBKDF2WithHmacSHA1";
-    private static final SecureRandom RANDOM = new SecureRandom();
+    private static final int SIZE = 128; // size of the hash
+    private static final int ITERATIONS = 18; // number of iterations
+    private static final String ALGORITHM = "PBKDF2WithHmacSHA1"; // algorithm to use
+    private static final SecureRandom RANDOM = new SecureRandom(); // random number generator
 
+    /**
+     * Returns true if the given password token matches the stored hash.
+     *
+     * @param password the password to check
+     * @param token    the password token to check against
+     *
+     * @return whether the password token matches the stored hash
+     *
+     */
     public static boolean authenticate(char[] password, String token) {
         byte[] hash = Base64.getUrlDecoder().decode(token);
         byte[] salt = Arrays.copyOfRange(hash, 0, SIZE / 8); // extract salt
         byte[] check = pbkdf2(password, salt); // derive key from password
         int zero = 0; // count zero bytes at the end of the hash
 
+        // compare the hash with the derived key
         for (int idx = 0; idx < check.length; ++idx)
             zero |= hash[salt.length + idx] ^ check[idx];
 
         return zero == 0;
     }
 
+    /**
+     * Returns a derived key from the given passcode and salt.
+     *
+     * @param passcode the passcode to derive the key from
+     * @param salt     the salt to use
+     *
+     * @return the derived key
+     *
+     */
     public static byte[] pbkdf2(char[] passcode, byte[] salt) {
         KeySpec spec = new PBEKeySpec(passcode, salt, ITERATIONS, SIZE);
 
@@ -57,7 +76,7 @@ public class Authenticate {
         RANDOM.nextBytes(salt);
 
         byte[] derivedKey = pbkdf2(password, salt); // derive the key
-        
+
         // create a hash array with length of salt + derived key
         byte[] hash = new byte[salt.length + derivedKey.length];
 
